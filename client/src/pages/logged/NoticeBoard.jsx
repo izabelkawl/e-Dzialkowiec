55de84441f7f53fd1d095cd558e00a6fcd262304
Iza <izabelawlazlo9@gmail.com>
2020-12-31 00:34:59 +0100

    basic modal insert in noticeboard

--- client/src/pages/logged/NoticeBoard.jsx
+++ client/src/pages/logged/NoticeBoard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect, Component} from "react";
 import api from "../../api";
 import styled from 'styled-components';
 import { Button, Form, Modal } from 'react-bootstrap';
@@ -50,45 +50,97 @@ const HeaderDiv = styled.div`
   font-size: 26px;
 `
 
-function AddAnnouncement(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Dodaj ogłoszenie
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-      <Form>
-  <Form.Group controlId="exampleForm.ControlInput1">
-    <Form.Label>Tytuł</Form.Label>
-    <Form.Control type="text"/>
-  </Form.Group>
-  <Form.Group controlId="exampleForm.ControlInput1">
-    <Form.Label>Użytkownik</Form.Label>
-    <Form.Control type="text" placeholder="karol@example.com" disabled/>
-  </Form.Group>
-  <Form.Group controlId="exampleForm.ControlTextarea1">
-    <Form.Label>Treść</Form.Label>
-    <Form.Control as="textarea" rows={3} />
-  </Form.Group>
-  <Form.Group>
-    <Form.File id="exampleFormControlFile1" label="Dodaj zdjęcie*" />
-  </Form.Group>
-</Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button style={BlueButtonStyle} >Dodaj</Button>
-        <Button style={RedButtonStyle} onClick={props.onHide}>Zamknij</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
+  class AddAnnouncement extends Component {
+    constructor(props) {
+      super(props);
+      
+      this.state = {
+        title: '',
+        user_id: '',
+        content: '',
+        image: ''
+    }
+      this.handleChangeInputTitle = this.handleChangeInputTitle.bind(this);
+      this.handleChangeInputUserId = this.handleChangeInputUserId.bind(this);
+      this.handleChangeInputContent = this.handleChangeInputContent.bind(this);
+      this.handleChangeInputImage = this.handleChangeInputImage.bind(this);
+  
+    }handleChangeInputTitle = async event => {
+      const title = event.target.value
+      this.setState({ title })
+  }
+  handleChangeInputUserId = async event => {
+    const user_id = event.target.value
+    this.setState({ user_id })
+  }
+  handleChangeInputContent = async event => {
+      const content = event.target.value
+      this.setState({ content })
+  }
+  handleChangeInputImage = async event => {
+  const image = event.target.value
+  this.setState({  image })
+  }
+
+  handleIncludeTable = async () => {
+      const { title, user_id, content, image } = this.state
+      const payload = { title, user_id, content, image }
+      console.log(payload)
+      await api.insertTable(payload).then(res => {
+          window.alert(`Table inserted successfully`)
+          this.setState({
+            title: '',
+            user_id: '',
+            content: '',
+            image: ''
+          })
+      })
+  }
+
+    render(){ 
+      const { title, user_id, content } = this.state
+   return (
+      <Modal
+        {...this.props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Dodaj wątek
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form>
+    <Form.Group controlId="exampleForm.ControlInput1">
+      <Form.Label>Tytuł</Form.Label>
+      <Form.Control type="text" value={title} onChange={this.handleChangeInputTitle}/>
+    </Form.Group>
+    <Form.Group controlId="exampleForm.ControlInput1">
+      <Form.Label>Użytkownik</Form.Label>
+      <Form.Control type="text"value={user_id}  onChange={this.handleChangeInputUserId} />
+    </Form.Group>
+    <Form.Group controlId="exampleForm.ControlTextarea1">
+      <Form.Label>Treść</Form.Label>
+      <Form.Control as="textarea" value={content} onChange={this.handleChangeInputContent} rows={3} />
+    </Form.Group>
+    {/* <Form.Group controlId="exampleForm.ControlTextarea1">
+      <Form.Label>Treść</Form.Label>
+      <Form.Control as="textarea"value={image}  onChange={this.handleChangeInputImage} rows={3} />
+    </Form.Group> */}
+   
+  </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          
+          <Button style={RedButtonStyle} onClick={this.props.onHide}>Zamknij</Button>
+          <Button style={BlueButtonStyle} onClick={this.handleIncludeTable}>Dodaj</Button>
+        </Modal.Footer>
+      </Modal>
+    )
+   }
+  }
 
 const NoticeBoard = () => {
     const [tables, setTables] = useState([]);
