07812df6f2c8f311178b1c3887ee9d1db4bf8ed6
Iza <izabelawlazlo9@gmail.com>
2021-02-22 16:01:49 +0100

    .

--- server/components/serverHTTP.js
+++ server/components/serverHTTP.js
@@ -19,10 +19,15 @@ import paymentdetailRouter from "../routes/paymentdetail-router.js";
 import managementRouter from "../routes/management-router.js";
 import announcementRouter from "../routes/announcement-router.js";
 import actRouter from "../routes/act-router.js";
-
+const __dirname = path.resolve();
 const app = express();
 
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.use('/uploads', express.static('uploads'));
 
 // Middlewares definition
